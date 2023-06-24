f0a8c7f95b4be57bd1f794068e48bb34e301f57b
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-24 18:04:48 +0200

    Work without data

--- src/main/java/com/ocr/mediscreen_mdb/Mediscreen_mdbApplication.java
+++ src/main/java/com/ocr/mediscreen_mdb/Mediscreen_mdbApplication.java
@@ -2,6 +2,8 @@ package com.ocr.mediscreen_mdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
