5ab3aa60bca7363afcd68e2b5a53a8223cc33d9e
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-29 17:07:58 +0200

    Refacto - Avant Docker

--- src/main/java/com/ocr/mediscreen_mdb/controller/PatientHistoryController.java
+++ src/main/java/com/ocr/mediscreen_mdb/controller/PatientHistoryController.java
@@ -1,7 +1,6 @@
 package com.ocr.mediscreen_mdb.controller;
 
 import com.ocr.mediscreen_mdb.Mediscreen_mdbApplication;
-
 import com.ocr.mediscreen_mdb.model.PatientHistory;
 import com.ocr.mediscreen_mdb.service.PatientHistoryService;
 import org.slf4j.Logger;
@@ -12,55 +11,50 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 public class PatientHistoryController {
 
-        @Autowired
-        private PatientHistoryService patientHistoryService;
-        private final Logger logger = LoggerFactory.getLogger(Mediscreen_mdbApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(PatientHistoryController.class);
 
+    @Autowired
+    private PatientHistoryService patientHistoryService;
 
     @GetMapping(value = "/PatHistoryList")
-        public List<PatientHistory> patientList() {
-            return  patientHistoryService.findAll();
-        }
+    public List<PatientHistory> patientList() {
+        logger.info("Get All the Notes");
+        return patientHistoryService.findAll();
+    }
 
     @GetMapping(value = "/PatHistory/patid/{patId}")
     public List<PatientHistory> getListNotesByPatId(@PathVariable Long patId) {
+        logger.info("get list of notes with patient id : " +patId);
         return patientHistoryService.getListNotesByPatId(patId);
-
     }
+
     @GetMapping(value = "/PatHistory/noteid/{noteId}")
     public PatientHistory getNoteById(@PathVariable Long noteId) {
+        logger.info("Get Note Id: " +noteId);
         return patientHistoryService.getNoteById(noteId);
     }
 
-
     @PostMapping(value = "/PatHistory/add")
-        public PatientHistory addNote(@RequestBody PatientHistory patientHistory) {
-
-          return  patientHistoryService.addPatientHistory(patientHistory);
-        }
-
+    public PatientHistory addNote(@RequestBody PatientHistory patientHistory) {
+        logger.info("Note Added: ");
+        return patientHistoryService.addPatientHistory(patientHistory);
+    }
 
     @PutMapping("/PatHistory/update/{id}")
     public PatientHistory updateNoteById(@PathVariable Long id,
-                           @RequestBody PatientHistory patientNoteToUpdate)  {
+                                         @RequestBody PatientHistory patientNoteToUpdate) {
+        logger.info("Note updated : " +id);
         PatientHistory noteUpdated = patientHistoryService.getNoteById(id);
         noteUpdated.setNotes(patientNoteToUpdate.getNotes());
         return patientHistoryService.updateNoteById(noteUpdated);
     }
 
-
-    @DeleteMapping(value= "/PatHistory/delete/{noteId}")
+    @DeleteMapping(value = "/PatHistory/delete/{noteId}")
     public void deleteNoteById(@PathVariable Long noteId) {
-         patientHistoryService.deleteNoteById(noteId);
+        logger.info("Note deleted : " +noteId);
+        patientHistoryService.deleteNoteById(noteId);
     }
-
-    }
-
-
-
-
-
+}
